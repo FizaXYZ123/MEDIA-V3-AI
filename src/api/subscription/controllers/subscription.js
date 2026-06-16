bdb31aba0e27bf52173cdcdedc61ab001dd916db
FizaXYZ123 <Fiza@thexyzstudio.com>
2026-06-16 16:34:27 +0530

    change redirect url

--- src/api/subscription/controllers/subscription.js
+++ src/api/subscription/controllers/subscription.js
@@ -30,11 +30,11 @@ module.exports = {
     const isApp = platform === "app";
 
     const successUrl = isApp
-      ? `mozart://payment-success`
+      ? `exp://192.168.1.13:8081/payment-success`
       : `${process.env.FRONTEND_BASE_URL}/payment-success`;
 
     const cancelUrl = isApp
-      ? `mozart://payment-cancel`
+      ? `exp://192.168.1.13:8081/payment-cancel`
       : `${process.env.FRONTEND_BASE_URL}/payment-cancel`;
 
     const plan = await strapi.db.query("api::plan.plan").findOne({
